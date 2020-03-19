37c5f18a5973284ab51c8d222f82220b7b87bb82
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-03-19 18:13:47 -0400

    create enviroment tsconfig and run ts

---
+++ 1-introduction/basic.js
@@ -0,0 +1,12 @@
+"use strict";
+var a = 'Hello World!';
+console.log(a);
+// yarn tsc --init > create file tsconfig.json
+// yarn tsc introduction/basic.ts > transpile file .ts to .js
+// node introduction/basic.js > run file .js
+// execute with code runner
+// install code runner in vs code
+// npm install -g ts-node
+// use Ctrl + Alt + N to run file TS
+// execute with browser
+// yarn tsc -w > listening files .ts
---
+++ 1-introduction/basic.ts
@@ -0,0 +1,15 @@
+const a: string = 'Hello World!'
+
+console.log(a)
+
+// yarn tsc --init > create file tsconfig.json
+// yarn tsc introduction/basic.ts > transpile file .ts to .js
+// node introduction/basic.js > run file .js
+
+// execute with code runner
+// install code runner in vs code
+// npm install -g ts-node
+// use Ctrl + Alt + N to run file TS
+
+// execute with browser
+// yarn tsc -w > listening files .ts
